96f8b71d1ba9f5edb4e607ef9d33d77873e1f446
Eric Gao <emg222@cornell.edu>
2015-09-22 03:09:46 -0400

    preclass 09.22

--- 2015-09-22/workq/workq.c
+++ 2015-09-22/workq/workq.c
@@ -150,10 +150,13 @@ void workq_wait(workq_t* workq)
  */
 void workq_put(workq_t* workq, void* data)
 {
+    workq_lock( workq );
     task_t* task = (task_t*) malloc(sizeof(task_t));
     task->data = data;
     task->next = workq->tasks;
     workq->tasks = task;
+    workq_signal( workq );
+    workq_unlock( workq );
 }
 
 
@@ -165,12 +168,14 @@ void workq_put(workq_t* workq, void* data)
 void* workq_get(workq_t* workq)
 {
     void* result = NULL;
+    workq_lock( workq );
     if (workq->tasks) {
         task_t* task = workq->tasks;
         result = task->data;
         workq->tasks = task->next;
         free(task);
     }
+    workq_unlock( workq );
     return result;
 }
 
